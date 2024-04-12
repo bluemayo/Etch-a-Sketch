21d4bfe089a291aee6606e1c61a95cea475735ce
Dylan <152871121+bluemayo@users.noreply.github.com>
2024-04-12 11:04:37 +0800

    Added column and row generator with flexbox style

--- script.js
+++ script.js
@@ -1,8 +1,16 @@
-const BOX = 4;
+const BOX = 10;
 const container = document.querySelector(".container");
+const length = Math.round(800 / BOX) - 2; //-2 for border
 
 
 for (let i = 0; i < BOX; i++) {
-    let div = document.createElement("div");
-    container.appendChild(div);
+    let column = document.createElement("div");
+    column.classList.add("column");
+    container.appendChild(column);
+    for (let j = 0; j < BOX; j++) {
+        let row = document.createElement("div");
+        row.classList.add("row");
+        row.setAttribute("style", `min-height: ${length}px; min-width: ${length}px;`)
+        column.appendChild(row);
+    }
 }
