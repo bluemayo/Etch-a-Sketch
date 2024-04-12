00175f60df27d28d8cd16b70ae29e16e37e59aff
Dylan <152871121+bluemayo@users.noreply.github.com>
2024-04-12 12:05:10 +0800

    Rectified Validation and added hover effect

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 function renderGrid() {
-    let boxes;
+    let box;
     do {
-        boxes = prompt("Enter the Dimensions: ");
-    } while (isNaN(parseInt(boxes)))
-    const box = parseInt(boxes);
-    if (box > 100) {
-        alert("Dimensions cannot be more than 100!")
-    }
+        box = userPrompt();
+        if (box > 100) {
+            alert("Dimensions cannot be more than 100!")
+        }
+    } while (box > 100)
+
     const length = Math.round(800 / box) - 2; //-2 for border
     
     for (let i = 0; i < box; i++) {
@@ -26,6 +26,15 @@ function resetGrid() {
     while (container.firstChild) {
         container.removeChild(container.firstChild);
     }
+}
+
+function userPrompt() {
+    let boxes;
+    do {
+        boxes = prompt("Enter the Dimensions: ");
+    } while (isNaN(parseInt(boxes)))
+    
+    return parseInt(boxes);
 }
 
 const container = document.querySelector(".container");
